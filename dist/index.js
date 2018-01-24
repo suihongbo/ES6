d917aa5bbbd7a8170aff24db414c6fc13cb19dd4
suihongbo <1120282913@qq.com>
2018-01-24 16:23:25 +0800

    practice

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var a = 5;
+var b = 10;
+var c = 100;
+var d = 0;
+var e = 1000;
+var f = 1;
+var g = 1000;
+var o = 0;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+let a=5;
+let b=10;
+let c=100;
+let d=0;
+let e=1000;
+let f=1;
+let g=1000;
+let o=0;
